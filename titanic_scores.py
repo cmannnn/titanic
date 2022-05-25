e1071b094b813439a13b302b9bb2d9c4d304781a
cmannnn <cmurphy1465@gmail.com>
2022-05-25 08:07:02 -0400

    merge

---
+++ titanic_scores.py
@@ -0,0 +1,13 @@
+# titanic_scores
+
+# take 1
+features = ['Pclass', 'Sex', 'SibSp', 'Parch']
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+
+
+Scores: [0.49441323 0.46204236 0.48575205 0.39661489 0.42399915 0.49718308
+ 0.43704832 0.50835712 0.41053541 0.43704832]
+Mean: 0.45529939493397575
+Standard deviation: 0.03763732640010849
+
+
